e61332ad42941a5793ad62fa6df215ec2f7fac69
renansaatman <renansaatman@hotmail.com>
2025-08-26 15:07:41 -0300

    configs para sass e backoffices

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,13 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
+
+Rails.application.config.assets.precompile += %w[ admins_backoffice.js admins_backoffice.css
+                                                  users_backoffice.js users_backoffice.css ]
+
+Rails.application.config.assets.precompile += %w[ sb-admin-2.js sb-admin-2.css
+                                                  custom.js custom.css
+                                                  img.jpg ]
+
+Rails.application.config.assets.precompile += %w[ jquery-2.2.3/dist/jquery.js ]
